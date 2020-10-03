f64179d159ec35831d546179fbc1761c5f609aa1
Anish Bhatta <anish@Anishs-MacBook-Pro.local>
2020-10-03 13:33:49 +0530

    First commit

---
+++ src/main/java/com/example/WebAppDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("WEB-INF/applicationContext.xml")
+public class WebAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppDemoApplication.class, args);
+	}
+
+}
