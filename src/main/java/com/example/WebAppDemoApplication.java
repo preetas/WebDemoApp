1e17bbecc56b017f4ec18782f12a9605f754ef4b
preetas <88144614+preetas@users.noreply.github.com>
2021-08-20 10:00:06 +0530

    Update WebAppDemoApplication.java

--- src/main/java/com/example/WebAppDemoApplication.java
+++ src/main/java/com/example/WebAppDemoApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ImportResource("applicationContext.xml")
+@ComponentScan(basePackages= {"com.example.mvc"})
 public class WebAppDemoApplication {
 
 	public static void main(String[] args) {
